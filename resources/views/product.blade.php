0cc7f936b8609e304c1f52f4187daabb3f94ea0a
Hendra <hendra.putra002@yahoo.com>
2025-07-23 14:09:35 +0700

    update product image

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,7 +7,7 @@
         {{-- Product Cards --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
             @foreach ($product as $item)
-            <a href="{{ url('/product/'.$item['product_code']) }}" class="block">
+            <a href="{{ url('/'.$item['product_code']) }}" class="block">
                 <div class="bg-white border rounded-lg shadow-md p-4 text-center">
                     <img src="{{ asset('images/'.$item['image'])}}" alt="{{ $item['product_name'] }}" class="h-40 mx-auto mb-4 object-contain" />
                     <p class="text-sm font-semibold text-gray-800 uppercase">{{ $item['product_name'] }}</p>
